5d2d9ad49fc4de8e9f09d4ba15fac6cec106e4ec
unknown <nhub1913327@student.ctu.edu.vn>
2023-02-01 21:07:47 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors")
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,17 @@ app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."})
+});
+
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal server Error",
+    });
 });
 
 module.exports = app;
